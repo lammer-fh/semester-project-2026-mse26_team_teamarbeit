9b21832d27be72c6c171f091aec2c09aabfcec17
dani.zangerl <dani.zangerl@aon.at>
2026-05-29 10:27:26 +0000

    Remove unused repositories; Update Room Repository to find available rooms

--- Backend/src/main/java/team_teamarbeit/backend/repository/RoomRepository.java
+++ Backend/src/main/java/team_teamarbeit/backend/repository/RoomRepository.java
@@ -1,11 +1,21 @@
 package team_teamarbeit.backend.repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import team_teamarbeit.backend.entity.Room;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, UUID> {
-    
+    @Query("SELECT r FROM Rooms r WHERE r.roomType.id = :roomTypeId AND r.id NOT IN " +
+        "(SELECT b.room.id FROM Bookings b WHERE b.arrivalDate < :to AND b.departureDate > :from)")
+    List<Room> findAvailableRooms(
+        @Param("roomTypeId") UUID roomTypeId, 
+        @Param("from") LocalDate from, 
+        @Param("to") LocalDate to
+    );
 }
