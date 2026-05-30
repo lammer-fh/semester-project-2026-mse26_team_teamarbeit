2cef2fbb12b59f8474e37513952121eff5fa5b35
Xorok <9898278+Xorok@users.noreply.github.com>
2026-05-30 03:31:50 +0000

    Add coverImagePath to RoomTypeDto and update API documentation

--- Backend/src/main/java/team_teamarbeit/backend/dto/RoomTypeDto.java
+++ Backend/src/main/java/team_teamarbeit/backend/dto/RoomTypeDto.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.UUID;
 import lombok.Builder;
 import lombok.Data;
+import team_teamarbeit.backend.entity.RoomImage;
 import team_teamarbeit.backend.entity.RoomType;
 
 @Data
@@ -13,17 +14,19 @@ public class RoomTypeDto {
     private String name;
     private String description;
     private Double pricePerNight;
+    private String coverImagePath;
     private List<String> imagePaths;
     private List<RoomExtraDto> roomExtras;
 
     public static RoomTypeDto fromRoomType(RoomType roomType) {
         return RoomTypeDto.builder()
-            .id(roomType.getId())
-            .name(roomType.getName())
-            .description(roomType.getDescription())
-            .pricePerNight(roomType.getPricePerNight().doubleValue())
-            .imagePaths(roomType.getRoomImages().stream().map(image -> image.getImagePath()).toList())
-            .roomExtras(roomType.getRoomExtras().stream().map(RoomExtraDto::fromRoomExtra).toList())
-            .build();
+                .id(roomType.getId())
+                .name(roomType.getName())
+                .description(roomType.getDescription())
+                .pricePerNight(roomType.getPricePerNight().doubleValue())
+                .coverImagePath(roomType.getCoverImagePath())
+                .imagePaths(roomType.getRoomImages().stream().map(RoomImage::getImagePath).toList())
+                .roomExtras(roomType.getRoomExtras().stream().map(RoomExtraDto::fromRoomExtra).toList())
+                .build();
     }
 }
