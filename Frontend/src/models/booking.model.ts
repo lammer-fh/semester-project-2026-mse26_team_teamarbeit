d419e715e13f40d59021cb691cbc4b70333ea34f
Johann Hoffmann <johannhoffmann@gmx.at>
2026-06-09 22:29:07 +0200

    implemented booking endpoint, store & model; created first version of booking form & review booking details cards;

--- Frontend/src/models/booking.model.ts
+++ Frontend/src/models/booking.model.ts
@@ -1,1 +1,35 @@
+export interface BookingGuest {
+  firstName: string
+  lastName: string
+  email: string
+}
 
+export interface CreateBookingRequest {
+  roomId: string
+  from: string
+  to: string
+  breakfast: boolean
+  userId: string | null
+  guest: BookingGuest | null
+}
+
+export interface Booking {
+  id: string
+  userId: string
+  roomId: string
+  from: string
+  to: string
+  breakfast: boolean
+  duration: number
+  totalPrice: number
+}
+
+export interface BookingListResponse {
+  bookings: Booking[]
+  page: {
+    size: number
+    totalElements: number
+    totalPages: number
+    number: number
+  }
+}
